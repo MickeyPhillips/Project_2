2ea36b696da9f52e716d6d6a603e84fca3448c50
Mickey Phillips <phillipsmickey41@gmail.com>
2021-11-04 23:13:31 -0500

    Created File Structure

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const session = require('express-session');
+
+
+app.listen(PORT, () => {
+    console.log(`Running on PORT ${PORT}!`);
+})
