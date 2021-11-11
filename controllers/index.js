70b8cd663b6c11ae224e13e87d30348d91adf40f
Mickey Phillips <phillipsmickey41@gmail.com>
2021-11-11 16:16:16 -0600

    readded tailwind, commented out the sequelize stuff

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
+const homeRoutes = require('./home-routes')
 
-router.get('/', (req, res) => {
-  res.render('homepage');
-});
+router.use('/', homeRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
-const routes = require('./controllers/');
+const routes = require('./controllers/index');
 const hbs = exphbs.create({});
 
 const app = express();
@@ -11,18 +10,18 @@ const PORT = process.env.PORT || 3000;
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// I commented the db stuff out b/c it wouldn't let me run the server with the database commands without the database
+// const sess = {
+//     secret: 'Super secret secret',
+//     cooke: {},
+//     resave: false,
+//     saveUninitialize: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
 
-const sess = {
-    secret: 'Super secret secret',
-    cooke: {},
-    resave: false,
-    saveUninitialize: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
-
-app.use(session(sess));
+// app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -31,7 +30,8 @@ app.set('view engine', 'handlebars');
 
 app.use(routes);
 
-sequelize.sync({force: false}).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
-})
+// sequelize.sync({force: false}).then(() => {
+//     app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
+// })
+app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
 
