b03bf70571acc94c2005e1ad0b2f80665cafb78f
nguyen-william93 <=>
2021-11-10 22:51:00 -0600

    set up server.js and session and handlebars path

--- server.js
+++ server.js
@@ -2,13 +2,33 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
+const routes = require('./controllers/');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cooke: {},
+    resave: false,
+    saveUninitialize: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-app.listen(PORT, () => {
-    console.log(`Running on PORT ${PORT}!`);
+app.use(routes);
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
 })
