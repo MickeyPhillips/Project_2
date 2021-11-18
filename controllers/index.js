5056cc6f71bb2d1287acfcd1b845cebaf02bb29a
Clayton Miller <cbmiller4897@gmail.com>
2021-11-17 22:18:12 -0600

    created dashboard page

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const homeRoutes = require('./home-routes')
 // const dashboardRoute = require('./dashboard_routes');
 const apiRoutes = require('./api');
+const { ExpressHandlebars } = require('express-handlebars');
 
 
 router.use('/api', apiRoutes);
@@ -9,3 +10,5 @@ router.use('/', homeRoutes);
 // router.use('/login', dashboardRoute)
 
 module.exports = router;
+
+// require('express').Router().use()
