c2275cd884465cc3f69c8084e075240b04d61a1c
nguyen-william93 <=>
2021-11-18 22:11:52 -0600

    add functionality to the logout button

---
+++ public/js/log-out.js
@@ -0,0 +1,15 @@
+async function logout(){
+    const response = await fetch('/api/employee/logout',
+    {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json'}
+    });
+
+    if (response.ok){
+        document.location.replace('/');
+    } else {
+        alert(response.status);
+    }
+}
+
+document.querySelector('#logout').addEventListener('click', logout);
