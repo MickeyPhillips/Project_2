951998c050dae30d88ca194d0357099b17c2c634
nguyen-william93 <=>
2021-11-22 10:37:29 -0600

    fix bug

--- controllers/index.js
+++ controllers/index.js
@@ -7,6 +7,10 @@ router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoute)
 
+router.use((req, res) => {
+    res.status(404).end();
+  });
+
 module.exports = router;
 
 // require('express').Router().use()
