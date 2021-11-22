0b64df407adb4cd74ac9b0aba5b3ef7814ce2995
Mickey Phillips <phillipsmickey41@gmail.com>
2021-11-22 14:04:32 -0600

    updated heroku:

--- controllers/api/employee-routes.js
+++ controllers/api/employee-routes.js
@@ -91,10 +91,7 @@ router.post('/login', (req, res) => {
                 res.json({ employee: employeeDB, message: 'You are now logged in'})
             })
         }
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+    })
 });
 
 //logout
