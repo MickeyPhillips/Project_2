b0bfc2cc9b0dfb07c6418c96b0e0fd4b4eebcb15
Mickey Phillips <phillipsmickey41@gmail.com>
2021-11-22 03:48:10 -0600

    routes fixed

--- controllers/api/employee-routes.js
+++ controllers/api/employee-routes.js
@@ -91,7 +91,10 @@ router.post('/login', (req, res) => {
                 res.json({ employee: employeeDB, message: 'You are now logged in'})
             })
         }
-    })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 //logout
