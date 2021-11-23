6eb79aa16520ed5713aa13b8e5ec922cc8dc1385
nguyen-william93 <=>
2021-11-22 22:11:49 -0600

    change force to true

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.set('view engine', 'handlebars');
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
 	app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
 // app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
